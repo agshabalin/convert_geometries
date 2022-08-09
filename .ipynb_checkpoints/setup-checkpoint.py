49da7e2614464c4985b31dda6a3424c9fe6d74a6
Anatoly Shabalin <shabalin@max-p3a003.desy.de>
2022-08-09 20:00:19 +0200

    Add existing project files to Git

--- .ipynb_checkpoints/setup-checkpoint.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='convert_geometries',
-    version='0.1',
-    packages=['convert_geometries',],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.md').read(),
-)
